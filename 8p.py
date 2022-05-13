1d23a06390c2e021d09cf78794e049404e9056da
DeronJMartin <deronudemartin@gmail.com>
2022-05-12 23:02:11 -0700

    Added Misplaced Tile Distance

---
+++ 8p.py
@@ -0,0 +1,12 @@
+# Define the distance function for the misplaced tile heuristic
+def misplacedTileDist(puzzleSize, puzzleState, goalState):
+    distance = 0
+
+    # Go through each piece of the current state and goal state and compare them
+    # Increment distance if they are not the same
+    for i in range(puzzleSize):
+        for j in range(puzzleSize):
+            if (goalState[i][j] != puzzleState[i][j]):
+                distance += 1
+
+    return distance

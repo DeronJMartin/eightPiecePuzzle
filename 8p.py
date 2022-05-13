8e5d9f40c72486899aeab7e2c7392a72c48bed69
DeronJMartin <deronudemartin@gmail.com>
2022-05-13 02:23:00 -0700

    Added input error detection

--- 8p.py
+++ 8p.py
@@ -18,8 +18,8 @@ def manhattanTileDistance(puzzleSize, puzzleState, goalState):
 
     """
     I didn't bother figuring out an algorithm with less
-    time complexity for generalizing manhattan distance
-    Its still pretty low for puzzle size 2,3,4
+    time complexity than O(n^^4) for generalizing manhattan distance
+    as its still pretty low for puzzle size 2,3,4
     """
 
     # Iterate through each piece of the goal state
@@ -34,3 +34,32 @@ def manhattanTileDistance(puzzleSize, puzzleState, goalState):
                     if (goalState[i][j] == puzzleState[ii][jj]):
                         distance += (abs(i - ii) + abs(j - jj))
 
+# Define driver function
+def driver(puzzleSize = 3, puzzleState = [[4,8,1],[3,0,5],[7,6,2]], goalState = [[1,2,3],[4,5,6],[7,8,0]]):
+    
+    # Detect errors in input
+    # Detect error in puzzle size
+    if (puzzleSize not in [2,3,4]):
+        print("This can only solve for 3-piece, 8-piece, or 15-piece puzzles!")
+
+    # Detect error in puzzle pieces
+    puzzlePieces = set()
+    for i in puzzleState:
+        for j in i:
+            puzzlePieces.add(j)
+    correctPuzzlePieces = {0, 1, 2, 3, 4, 5, 6, 7, 8}
+    if (puzzlePieces != correctPuzzlePieces):
+        print("Error! Incorrect input for puzzle state!")
+
+    # Detect error in goal pieces
+    goalPieces = set()
+    for i in goalState:
+        for j in i:
+            goalPieces.add(j)
+    correctGoalPieces = {0, 1, 2, 3, 4, 5, 6, 7, 8}
+    if (goalPieces != correctGoalPieces):
+        print("Error! Incorrect input for goal state!")
+
+
+if __name__ == "__main__":
+    print("Test")
